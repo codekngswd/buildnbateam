004fc94e59d06b94b0eefb8070bdfdcb818454be
codekngswd <codekngswd@gmail.com>
2018-08-11 16:02:27 -0400

    Initial project structure for Build NBA Team

---
+++ src/player-list.js
@@ -0,0 +1,208 @@
+/**
+ * This list has been put together through the link below.
+ * https://bleacherreport.com/articles/2753686-player-position-rankings-nbas-top-100-players-for-seasons-2nd-half
+ */
+
+const players = {
+    player1: {
+        name: "Stephen Curry",
+        image: "/images/stephencurry.png",
+        position: "PG",
+        price: 5,
+        status: "available"
+    },
+
+    player2: {
+        name: "Russell Westbrook",
+        image: "/images/russellwestbrook.png",
+        position: "PG",
+        price: 4,
+        status: "available"
+    },
+
+    player3: {
+        name: "Chris Paul",
+        image: "/images/chrispaul.png",
+        position: "PG",
+        price: 3,
+        status: "available"
+    },
+
+    player4: {
+        name: "Kyrie Irving",
+        image: "/images/kyrieirving.png",
+        position: "PG",
+        price: 2,
+        status: "available"
+    },
+
+    player5: {
+        name: "Damian Lillard",
+        image: "/images/.png",
+        position: "PG",
+        price: 1,
+        status: "available"
+    },
+
+    player6: {
+        name: "James Harden",
+        image: "/images/jamesharden.png",
+        position: "SG",
+        price: 5,
+        status: "available"
+    },
+
+    player7: {
+        name: "Victor Oladipo",
+        image: "/images/victoroladipo.png",
+        position: "SG",
+        price: 4,
+        status: "available"
+    },
+
+    player8: {
+        name: "Klay Thompson",
+        image: "/images/klaythompson.png",
+        position: "SG",
+        price: 3,
+        status: "available"
+    },
+
+    player9: {
+        name: "Bradley Beal",
+        image: "/images/bradleybeal.png",
+        position: "SG",
+        price: 2,
+        status: "available"
+    },
+
+    player10: {
+        name: "Gary Harris",
+        image: "/images/garyharris.png",
+        position: "SG",
+        price: 1,
+        status: "available"
+    },
+
+    player11: {
+        name: "LeBron James",
+        image: "/images/lebronjames.png",
+        position: "SF",
+        price: 5,
+        status: "available"
+    },
+
+    player12: {
+        name: "Kevin Durant",
+        image: "/images/kevindurant.png",
+        position: "SF",
+        price: 4,
+        status: "available"
+    },
+
+    player13: {
+        name: "Kawhi Leonard",
+        image: "/images/kawhileonard.png",
+        position: "SF",
+        price: 3,
+        status: "available"
+    },
+
+    player14: {
+        name: "Jimmy Butler",
+        image: "/images/jimmybutler.png",
+        position: "SF",
+        price: 2,
+        status: "available"
+    },
+
+    player15: {
+        name: "Paul George",
+        image: "/images/paulgeorge.png",
+        position: "SF",
+        price: 1,
+        status: "available"
+    },
+
+    player16: {
+        name: "Giannis Antetokounmpo",
+        image: "/images/giannisantetokounmpo.png",
+        position: "PF",
+        price: 5,
+        status: "available"
+    },
+
+    player17: {
+        name: "Anthony Davis",
+        image: "/images/anthonydavis.png",
+        position: "PF",
+        price: 4,
+        status: "available"
+    },
+
+    player18: {
+        name: "Draymond Green",
+        image: "/images/draymondgreen.png",
+        position: "PF",
+        price: 3,
+        status: "available"
+    },
+
+    player19: {
+        name: "Kristaps Porzingis",
+        image: "/images/kristapsporzingis.png",
+        position: "PF",
+        price: 2,
+        status: "available"
+    },
+
+    player20: {
+        name: "Blake Griffin",
+        image: "/images/blakegriffin.png",
+        position: "PF",
+        price: 1,
+        status: "available"
+    },
+
+    player21: {
+        name: "DeMarcus Cousins",
+        image: "/images/demarcuscousins.png",
+        position: "C",
+        price: 5,
+        status: "available"
+    },
+
+    player22: {
+        name: "Joel Embiid",
+        image: "/images/joelembiid.png",
+        position: "C",
+        price: 4,
+        status: "available"
+    },
+
+    player23: {
+        name: "Al Horford",
+        image: "/images/alhorford.png",
+        position: "C",
+        price: 3,
+        status: "available"
+    },
+
+    player24: {
+        name: "Nikola Jokic",
+        image: "/images/nikolajokic.png",
+        position: "C",
+        price: 2,
+        status: "available"
+    },
+
+    player25: {
+        name: "Karl-Anthony Towns",
+        image: "/images/karlanthonytowns.png",
+        position: "C",
+        price: 1,
+        status: "available"
+    }
+};
+
+export default players;
